402a3d21856a4ed90e068accb6b2b24bda611438
sriram1661 <sriram1661@gmail.com>
2022-11-02 08:59:59 +0530

    Sprint1 completed

---
+++ Project Development Phase/Sprint 1/myapp/api/urls.py
@@ -0,0 +1,12 @@
+# from django.urls import path
+# from . import views
+
+# # urlpatterns = [
+# #     path('login',views.login_user),
+# #     path('user',views.get_user_detail),
+# #     path('register',views.register_user),
+# #     path('getotp',views.get_otp),
+# #     path('verifyotp',views.verify_otp),
+# #     path('updatepassword',views.update_password),
+# #     path('updatephno',views.update_phonenumber),
+# # ]
